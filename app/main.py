248d7e1d9a88967d9994a9b4a66f91e526fa368a
Hitesh Tara <mukadder@Hiteshs-iMac.local>
2025-11-13 22:11:00 -0500

    mac test

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from api.routes_etl import router as etl_router
+from api.routes_summary import router as summary_router
+
 
 app=FastAPI()
 @app.get('/')
 def home(): return {'status':'prod-ready'}
 app.include_router(etl_router, prefix="/etl")
+app.include_router(summary_router, prefix="/etl")
